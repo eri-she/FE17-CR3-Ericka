aa4ef937bb09b0348a3cf44980c208975a7c6155
Ericka Angeles <ericka.angelesgarcia@gmail.com>
2022-10-14 12:35:42 +0200

    added details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../products/products';
+import { IProducts } from '../products/interfacep';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent implements OnInit {
-
-  constructor() { }
+  product: IProducts = {} as IProducts;
+  id: number = 0;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['productId'];
+      this.product = products[this.id];
+    });
   }
-
 }
