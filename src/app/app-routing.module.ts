ed21b3fe6917dad3cf6a6a2299aa5acf9dbbf511
Ericka Angeles <ericka.angelesgarcia@gmail.com>
2022-10-14 11:47:07 +0200

    added links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { CartComponent } from './cart/cart.component';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutUsComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'products/:productId', component: DetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
