8e9d6a273f21f730d6ad4dc01494d861460cc98a
Ericka Angeles <ericka.angelesgarcia@gmail.com>
2022-10-15 13:35:17 +0200

    final pages

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -5,20 +5,22 @@ export const products = [
     image: `https://cdn.pixabay.com/photo/2012/07/09/07/16/thali-51996__340.jpg`,
     description:
       'A Thali is a platter that is made up of several different dishes to create a perfectly balanced meal',
-    availability: true,
+    availability: false,
   },
   {
     name: 'Curry',
     price: 15,
     image: `https://cdn.pixabay.com/photo/2022/06/07/20/52/curry-7249247__340.jpg`,
-    description: 'Cooked with chicken and peanut butter',
+    description:
+      ' curry is an Indian gravy or sauce that is used in tandem with meat, tofu, or vegetables. It’s served rice, most popularly Basmati rice, and contains many different kinds of spices',
     availability: true,
   },
   {
     name: 'Pancake',
     price: 12,
     image: `https://cdn.pixabay.com/photo/2017/01/16/17/45/pancake-1984716_960_720.jpg`,
-    description: 'Pancakes with fruits',
+    description:
+      'A pancake is a thin, flat, circular piece of cooked batter made from milk, flour, and egg',
     availability: true,
   },
   {
@@ -26,6 +28,51 @@ export const products = [
     price: 13,
     image: `https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_960_720.jpg`,
     description: 'A creamy dessert of espresso-soaked ladyfingers ',
+    availability: true,
+  },
+  {
+    name: `Burger`,
+    price: 18,
+    image: `https://cdn.pixabay.com/photo/2014/10/19/20/59/hamburger-494706__340.jpg`,
+    description:
+      'burger, is a food consisting of fillings—usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll.',
+    availability: true,
+  },
+  {
+    name: 'Salad',
+    price: 13,
+    image: `https://cdn.pixabay.com/photo/2016/03/10/18/44/top-view-1248955__340.jpg`,
+    description: 'A creamy dessert of espresso-soaked ladyfingers ',
     availability: false,
+  },
+  {
+    name: 'Breakfast',
+    price: 9,
+    image: `https://cdn.pixabay.com/photo/2016/11/06/23/31/breakfast-1804457_960_720.jpg`,
+    description: 'A creamy dessert of espresso-soaked ladyfingers ',
+    availability: true,
+  },
+  {
+    name: `Salmon`,
+    price: 17,
+    image: `https://cdn.pixabay.com/photo/2016/03/05/19/02/salmon-1238248__340.jpg`,
+    description:
+      'Our baked salmon recipe delivers soft, juicy and tender salmon on the inside with charred crisp edges. A delicious restaurant-quality dinner!  ',
+    availability: true,
+  },
+  {
+    name: 'Salad',
+    price: 13,
+    image: `https://cdn.pixabay.com/photo/2016/03/10/18/44/top-view-1248955__340.jpg`,
+    description: 'A creamy dessert of espresso-soaked ladyfingers ',
+    availability: false,
+  },
+  {
+    name: 'Breakfast',
+    price: 9,
+    image: `https://cdn.pixabay.com/photo/2016/11/06/23/31/breakfast-1804457_960_720.jpg`,
+    description:
+      'As we all know, breakfast is the most important meal of the day - so why not make it delicious!',
+    availability: true,
   },
 ];
